f8adae9a147899836c546ad38b8ff95b405e98b9
ERIK M. DODD <emdodd27@gmail.com>
2022-10-11 11:11:37 -0700

    moved LogIn to App.js. Added rows and cols to aboutTeam

--- src/components/AboutTeam.js
+++ src/components/AboutTeam.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 
@@ -7,85 +10,96 @@ class AboutTeam extends React.Component {
 	render() {
 		return (
 			<>
-				<Card style={{ width: '18rem' }}>
-					<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-					<Card.Body>
-						<Card.Title>Cianenry Danan</Card.Title>
-						<Card.Text>
-							Some quick example text to build on the card title and make up the
-							bulk of the card's content.
-						</Card.Text>
-					</Card.Body>
-					<ListGroup className="list-group-flush">
+				<Container>
+					<Row>
+						<Col sm={6}> 
+							<Card style={{ width: '18rem' }}>
+								<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+								<Card.Body>
+									<Card.Title>Cianenry Danan</Card.Title>
+									<Card.Text>
+										Some quick example text to build on the card title and make up the
+										bulk of the card's content.
+									</Card.Text>
+								</Card.Body>
+								<ListGroup className="list-group-flush">
 
-						<ListGroup.Item>Favorite Artists:  </ListGroup.Item>
-						<ListGroup.Item>Favorite Venue: </ListGroup.Item>
-					</ListGroup>
-					<Card.Body>
-						<Card.Link href="#">GitHub</Card.Link>
-						<Card.Link href="#">LinkedIn</Card.Link>
-					</Card.Body>
-				</Card>
+									<ListGroup.Item>Favorite Artists:  </ListGroup.Item>
+									<ListGroup.Item>Favorite Venue: </ListGroup.Item>
+								</ListGroup>
+								<Card.Body>
+									<Card.Link href="#">GitHub</Card.Link>
+									<Card.Link href="#">LinkedIn</Card.Link>
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col sm={6}>
+							<Card style={{ width: '18rem' }}>
+								<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+								<Card.Body>
+									<Card.Title>Chris Hollis</Card.Title>
+									<Card.Text>
+										Some quick example text to build on the card title and make up the
+										bulk of the card's content.
+									</Card.Text>
+								</Card.Body>
+								<ListGroup className="list-group-flush">
 
-				<Card style={{ width: '18rem' }}>
-					<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-					<Card.Body>
-						<Card.Title>Chris Hollis</Card.Title>
-						<Card.Text>
-							Some quick example text to build on the card title and make up the
-							bulk of the card's content.
-						</Card.Text>
-					</Card.Body>
-					<ListGroup className="list-group-flush">
+									<ListGroup.Item>Favorite Artists:  </ListGroup.Item>
+									<ListGroup.Item>Favorite Venue: </ListGroup.Item>
+								</ListGroup>
+								<Card.Body>
+									<Card.Link href="#">GitHub</Card.Link>
+									<Card.Link href="#">LinkedIn</Card.Link>
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row>
+					<Row>
+						<Col sm={6}>
+							<Card style={{ width: '18rem' }}>
+								<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+								<Card.Body>
+									<Card.Title>Erik Dodd</Card.Title>
+									<Card.Text>
+										Some quick example text to build on the card title and make up the
+										bulk of the card's content.
+									</Card.Text>
+								</Card.Body>
+								<ListGroup className="list-group-flush">
 
-						<ListGroup.Item>Favorite Artists:  </ListGroup.Item>
-						<ListGroup.Item>Favorite Venue: </ListGroup.Item>
-					</ListGroup>
-					<Card.Body>
-						<Card.Link href="#">GitHub</Card.Link>
-						<Card.Link href="#">LinkedIn</Card.Link>
-					</Card.Body>
-				</Card>
+									<ListGroup.Item>Favorite Artists:  </ListGroup.Item>
+									<ListGroup.Item>Favorite Venue: </ListGroup.Item>
+								</ListGroup>
+								<Card.Body>
+									<Card.Link href="#">GitHub</Card.Link>
+									<Card.Link href="#">LinkedIn</Card.Link>
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col sm={6}>
+							<Card style={{ width: '18rem' }}>
+								<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+								<Card.Body>
+									<Card.Title>Julian Gallegos</Card.Title>
+									<Card.Text>
+										Some quick example text to build on the card title and make up the
+										bulk of the card's content.
+									</Card.Text>
+								</Card.Body>
+								<ListGroup className="list-group-flush">
 
-				<Card style={{ width: '18rem' }}>
-					<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-					<Card.Body>
-						<Card.Title>Erik Dodd</Card.Title>
-						<Card.Text>
-							Some quick example text to build on the card title and make up the
-							bulk of the card's content.
-						</Card.Text>
-					</Card.Body>
-					<ListGroup className="list-group-flush">
-
-						<ListGroup.Item>Favorite Artists:  </ListGroup.Item>
-						<ListGroup.Item>Favorite Venue: </ListGroup.Item>
-					</ListGroup>
-					<Card.Body>
-						<Card.Link href="#">GitHub</Card.Link>
-						<Card.Link href="#">LinkedIn</Card.Link>
-					</Card.Body>
-				</Card>
-
-				<Card style={{ width: '18rem' }}>
-					<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-					<Card.Body>
-						<Card.Title>Julian Gallegos</Card.Title>
-						<Card.Text>
-							Some quick example text to build on the card title and make up the
-							bulk of the card's content.
-						</Card.Text>
-					</Card.Body>
-					<ListGroup className="list-group-flush">
-
-						<ListGroup.Item>Favorite Artists:  </ListGroup.Item>
-						<ListGroup.Item>Favorite Venue: </ListGroup.Item>
-					</ListGroup>
-					<Card.Body>
-						<Card.Link href="#">GitHub</Card.Link>
-						<Card.Link href="#">LinkedIn</Card.Link>
-					</Card.Body>
-				</Card>
+									<ListGroup.Item>Favorite Artists:  </ListGroup.Item>
+									<ListGroup.Item>Favorite Venue: </ListGroup.Item>
+								</ListGroup>
+								<Card.Body>
+									<Card.Link href="#">GitHub</Card.Link>
+									<Card.Link href="#">LinkedIn</Card.Link>
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row>
+				</Container>
 			</>
 		)
 	}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import AboutTeam from './AboutTeam';
-import LogIn from './LogIn.js'
 import SignUp from './SignUp';
 
 class Main extends React.Component {
@@ -9,7 +8,6 @@ class Main extends React.Component {
   render(){
     return(
       <>
-      <LogIn/>
       <SignUp/>
       <AboutTeam/>
       </>
