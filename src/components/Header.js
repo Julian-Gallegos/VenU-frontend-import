27cb565c01fd52dc6a7411318a923d6fea6787f4
Cianenry Danan <cianenryd@gmail.com>
2022-10-11 18:22:20 -0700

    Structured artist search component, added navbar, added footer, created artist modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+
 class Header extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
       <>
-    
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">VenU</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/userprofile">Username</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
       </>
     )
   }
